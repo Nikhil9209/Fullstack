e586c3d3233b689e1ed6b8da4eebb16f11b7ce76
Nikhil Jangid <nikhiljangid1704@gmail.com>
2026-08-31 09:39:52 +0530

    more about normal function and declaring a  function iniside a variable

--- JS/Class Code/Day02/day2.js
+++ JS/Class Code/Day02/day2.js
@@ -15,28 +15,47 @@
 
 // Nikhil("nik","hi","l")
 
-function add(num1,num2){
-    const result = num1+num2;
-    return result ;
+// function add(num1,num2){
+//     const result = num1+num2;
+//     return result ;
+// }
+
+
+// const r =  add(2,5);
+// console.log(r);
+
+
+
+// function cartoon(){
+
+//     function cartoonInsideCartoon(){
+
+//         return 'Naruto';
+//     }
+//     return cartoonInsideCartoon; //  It is returning a fucntion .. and 
+
+// }
+
+// const anime = cartoon();
+// const ri = anime ();
+// console.log(ri);
+
+
+// age = 45;
+
+// console.log('Value  of age is ', age, 'Is allowed', isAlllowedToVote(age));
+// var age   =  24;
+
+// function isAllowedToVote(age){
+//     return age>=18;
+// }
+
+
+age = 45;
+
+console.log('Value  of age is ', age, 'Is allowed', isAlllowedToVote(age));
+var age   =  24;
+
+var isAllowedToVote = function (age){
+    return age>=18;
 }
-
-
-const r =  add(2,5);
-console.log(r);
-
-
-
-function cartoon(){
-
-    function cartoonInsideCartoon(){
-
-        return 'Naruto';
-    }
-    return cartoonInsideCartoon; //  It is returning a fucntion .. and 
-
-}
-
-const anime = cartoon();
-const ri = anime ();
-console.log(ri);
-
