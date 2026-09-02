5f689a6c314f41b85157ae8898e00f89a02bb4e8
Nikhil Jangid <nikhiljangid1704@gmail.com>
2026-09-02 09:57:56 +0530

    basics about js

---
+++ JS/Class Code/Day03/js-refresher.js
@@ -0,0 +1,16 @@
+// console.log("Hello World");
+
+//  user-profile..js --> it is a kebab case file name example in thiss the file name is written in lower case seprated by hyphens  
+
+
+
+// Topic:-Strict Mode 
+
+
+// 2009 ES5 addded new feature which breaks few things ..
+// Too keep old syste  running - new feature are turned off by default 
+
+// use strick --> always at top 
+// "no use strict"
+// JS --> Class , Modules
+
